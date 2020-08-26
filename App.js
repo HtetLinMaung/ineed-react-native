7dccd0cd45f6a0fcde9957e3ae3cf5d4c6e45783
Htet Lin Maung <jsthtet96@gmail.com>
2020-08-26 16:01:13 +0630

    project upload

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import NeedProvider from "./contexts/NeedProvider";
+import { StyleSheet } from "react-native";
+import { AppLoading } from "expo";
+import { useFonts } from "expo-font";
+import Navigator from "./components/Navigator";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    Roboto: require("native-base/Fonts/Roboto.ttf"),
+    Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+    ...Ionicons.font,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NeedProvider>
+      <Navigator />
+    </NeedProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
